e371783c36516d114ff9d0d08fe742d23a1044b2
Alexandre Vanier <alex.vanier@live.fr>
2021-11-02 12:01:00 -0400

    modification css

--- footer.php
+++ footer.php
@@ -12,9 +12,9 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-	<!--	<div class="site-info">
-		
-		</div>--><!-- .site-info -->
+		<div class="site-info">
+			<p>bajisbviba</p>
+		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
