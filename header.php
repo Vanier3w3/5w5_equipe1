be1afa60645ea2ebeb740c34944a977a5d7968fc
sabrine17 <sabrine.cheurfa@yahoo.ca>
2021-11-10 13:37:34 -0500

    yam modif

--- header.php
+++ header.php
@@ -31,7 +31,9 @@
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			
+				<h1 class="site-title">	<img src="wp-content/uploads/LogoTimSiteE.png" class="titre1" style="background-size:30px;"> </img><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				
 				<?php
 			else :
 				?>
