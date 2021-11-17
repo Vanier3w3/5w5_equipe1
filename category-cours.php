a931fc4a7fb3c29880c95c5bcb526e634528c231
Alexandre Vanier <alex.vanier@live.fr>
2021-11-16 22:26:11 -0500

    css cours

--- category-cours.php
+++ category-cours.php
@@ -10,7 +10,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main">	
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
@@ -19,39 +19,39 @@ get_header();
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			$precedent = 'XX';
-	
-			while ( have_posts() ) :
-				the_post();
-				convertirTitreCours($propriete);
-				if($precedent != $propriete['session']):
-					if($precedent != 'XX'):
-				echo '<h1>session:'. $precedent . '</h1>';
-				?>
-					</section>
+			<div id="contenuPage">
 				<?php
+				/* Start the Loop */
+				$precedent = 'XX';
+		
+				while ( have_posts() ) :
+					the_post();
+					convertirTitreCours($propriete);
+					if($precedent != $propriete['session']):
+						if($precedent != 'XX'):
+						?>
+							</section>
+					<?php
+						endif;
+					?>
+						<section class="sessionBloc">
+							
+				<?php
+						echo '<h1>session: '. $propriete['session'] . '</h1>';
 					endif;
-				?>
-					<section class="session_bloc">
-			<?php
-				endif;
-				get_template_part( 'template-parts/content', 'cours' );
-				$precedent = $propriete['session'];
+					get_template_part( 'template-parts/content', 'cours' );
+					$precedent = $propriete['session'];
+					
+				endwhile;
+				the_posts_navigation();
 				
-				
-			endwhile;
-			the_posts_navigation();
-            
-            else :
-                
-                get_template_part( 'template-parts/content', 'none' );
+				else :
+					
+					get_template_part( 'template-parts/content', 'none' );
 
-		endif;
-		?>
-
+			endif;
+			?>
+			</div>
 	</main><!-- #main -->
 
 <?php
@@ -61,7 +61,7 @@ get_footer();
 function convertirTitreCours(&$propriete){
 	$propriete['titre'] = get_the_title(); 
 	$propriete['titrePartiel'] = substr($propriete['titre'],8,-6);
-	$tPropriété['sigle'] = substr($tPropriété['titre'], 0, 7);
+	$propriete['sigle'] = substr($propriete['titre'], 0, 7);
 	$propriete['session'] = substr($propriete['titre'], 4,1);
 	$propriete['typeCours'] = get_field('type_de_cours');
 }
